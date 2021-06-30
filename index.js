fe6885dfbc3521d75518e328e21f1a318e50e749
Sandra Wazaz <sandrawazaz@gmail.com>
2021-06-29 20:35:13 -0400

    commit tree 72b64910d0f547c153c3ab0cc483c6c5beccee9e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+const employee = {
+name: "bean",
+streetAddress: "88 east 3rd"
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+const newObj = {...obj}
+return obj
+}
